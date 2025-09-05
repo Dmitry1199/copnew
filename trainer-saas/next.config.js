df02bb8a779cd28ccc94cb13f95f7951ff817629
Dmitry1199 <biletskiydmitriy96@gmail.com>
2025-09-05 21:47:36 +0300

    Update next.config.js

--- trainer-saas/next.config.js
+++ trainer-saas/next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  outputFileTracingExcludes: {
-    '*': [
-      'node_modules/@swc/core-linux-x64-gnu',
-      'node_modules/@swc/core-linux-x64-musl',
-      'node_modules/@esbuild/linux-x64',
-    ],
-  },
   eslint: {
-    ignoreDuringBuilds: true,
-    dirs: [],
+    ignoreDuringBuilds: true
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   },
   images: {
     unoptimized: true,
@@ -20,31 +12,31 @@ const nextConfig = {
       "source.unsplash.com",
       "images.unsplash.com",
       "ext.same-assets.com",
-      "ugc.same-assets.com",
+      "ugc.same-assets.com"
     ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "source.unsplash.com",
-        pathname: "/**",
+        pathname: "/**"
       },
       {
         protocol: "https",
         hostname: "images.unsplash.com",
-        pathname: "/**",
+        pathname: "/**"
       },
       {
         protocol: "https",
         hostname: "ext.same-assets.com",
-        pathname: "/**",
+        pathname: "/**"
       },
       {
         protocol: "https",
         hostname: "ugc.same-assets.com",
-        pathname: "/**",
-      },
-    ],
-  },
+        pathname: "/**"
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
